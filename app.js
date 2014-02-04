5a690d0c35a090119a4389a2e7a464e759a62129
Han Jing <hanqijing@gmail.com>
2014-02-04 15:07:19 -0800

    better port better info

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var path = require('path');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5678);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -29,6 +29,8 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/payload', routes.payload);
+app.post('/payload', routes.payload);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,10 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.payload = function(req, res){
+  console.log("fooooo");
+  console.log(req);
+  console.log(res);
+  res.render('index', { title: 'payload' });
+};
