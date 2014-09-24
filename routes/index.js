ec63fb235ff9e31acbff2c774f70ede4d6c546e3
Han Jing <hanqijing@gmail.com>
2014-09-23 20:21:21 -0700

    fix some route stuff

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,24 @@ exports.index = function(req, res){
 };
 
 exports.payload = function(req, res){
+  var http = require('http')
+    , req = http.IncomingMessage.prototype;
+
+  var _addHeaderLine = req._addHeaderLine;
+
+  //Patch ServerRequest to save unmodified copy of headers
+  req._addHeaderLine = function(field, value) {
+    var list = this.complete ?
+      (this.allTrailers || (this.allTrailers = [])) :
+      (this.allHeaders || (this.allHeaders = []));
+    list.push(field + ': ' + value);
+    _addHeaderLine.call(this, field, value);
+  };
+
   console.log("fooooo");
   console.log(req);
   console.log(res);
+  console.log("raw headers");
+  console.log(req._allHeaders.join('\r\n'));
   res.render('index', { title: 'payload' });
 };
